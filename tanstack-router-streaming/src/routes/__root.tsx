5fab2d56d3bb9278f05d0587107ed900155c9e80
Дмитрий Варламов <dmitrijvarlamov@Dmitrijs-MacBook-Pro.local>
2024-02-01 20:12:23 +0200

    feat: add stream for tanstack router

---
+++ tanstack-router-streaming/src/routes/__root.tsx
@@ -0,0 +1,66 @@
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import {Link, Outlet, createRootRouteWithContext, useRouter} from '@tanstack/react-router'
+import { DehydrateRouter } from '@tanstack/react-router-server/client'
+import React from "react";
+export const Route = createRootRouteWithContext<{
+    assets: React.ReactNode
+}>()({
+    component: RootComponent,
+    notFoundComponent: () => <h1 style={{color: "red"}}>Ooops...</h1>
+})
+
+function RootComponent() {
+    const router = useRouter();
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Vite App</title>
+            <script
+                type="module"
+                suppressHydrationWarning
+                dangerouslySetInnerHTML={{
+                    __html: `
+              import RefreshRuntime from "/@react-refresh"
+              RefreshRuntime.injectIntoGlobalHook(window)
+              window.$RefreshReg$ = () => {}
+              window.$RefreshSig$ = () => (type) => type
+              window.__vite_plugin_react_preamble_installed__ = true
+            `,
+                }}
+            />
+            {router.options.context.assets}
+            {/*<script type="module" src="/@vite/client" />*/}
+            {/*<script type="module" src="/src/entry.client.tsx" />*/}
+        </head>
+        <body>
+        <div>
+            <Link
+                to="/"
+                activeOptions={{ exact: true }}
+            >
+                Home
+            </Link>{' '}
+            <Link
+                to="/about"
+                activeOptions={{ exact: true }}
+            >
+                About
+            </Link>{' '}
+            <Link
+                to="/test"
+                activeOptions={{ exact: true }}
+            >
+                Test
+            </Link>
+        </div>
+        <hr />
+        <Outlet /> {/* Start rendering routes matches */}
+        <TanStackRouterDevtools position="bottom-right" />
+        <DehydrateRouter />
+        </body>
+        </html>
+    )
+}
---
+++ tanstack-router-streaming/src/server.tsx
@@ -0,0 +1,68 @@
+import {renderToPipeableStream, PipeableStream} from 'react-dom/server';
+import {render} from "./entry.server.tsx";
+import { isbot } from "isbot";
+import {
+    transformStreamWithRouter,
+} from "@tanstack/react-router-server/server";
+import {Writable} from "node:stream";
+export default {
+     async fetch(req: Request) {
+        const { app, router } = await render(req);
+
+         let statusCode = 200;
+
+         const isCrawler = isbot(req.headers.get("user-agent"));
+
+         const renderStream = await new Promise<PipeableStream>((resolve) => {
+             const stream = renderToPipeableStream(app, {
+                 onAllReady() {
+                     if (isCrawler) {
+                         console.log("onAllReady");
+                         resolve(stream);
+                     }
+                 },
+                 onShellReady() {
+                     if (!isCrawler) {
+                         console.log("onShellReady");
+                         resolve(stream);
+                     }
+                 },
+                 onShellError(error) {
+                     console.error("onShellError", error);
+                     statusCode = 500
+                 },
+                 onError(error) {
+                     statusCode = 500
+                     console.error("onError", error);
+                     // TODO: logger
+                 },
+             })
+         })
+
+         const didError = statusCode > 399 && statusCode < 600;
+
+         if (router.hasNotFoundMatch() && !didError) {
+             statusCode = 404
+         }
+
+         const { readable, writable } = new TransformStream();
+
+         // transform
+         renderStream
+             .pipe(transformStreamWithRouter(router))
+             .pipe(Writable.fromWeb(writable));
+
+         // “give up” after a timeout
+         setTimeout(() => {
+             renderStream.abort();
+         }, 10000);
+
+
+         return new Response(readable, {
+            status: statusCode,
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        });
+    },
+};
