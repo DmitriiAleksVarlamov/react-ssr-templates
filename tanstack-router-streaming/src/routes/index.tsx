1e77e22102a59dafb8924bf4c15412265a384594
Дмитрий Варламов <dmitrijvarlamov@Dmitrijs-MacBook-Pro.local>
2024-02-03 18:27:02 +0200

    frat: remove extra comments

--- tanstack-router-streaming/src/routes/index.tsx
+++ tanstack-router-streaming/src/routes/index.tsx
@@ -11,20 +11,3 @@ function Index() {
         </div>
     )
 }
-
-// import {createRoute} from "@tanstack/react-router";
-// import {rootRoute} from "./__root.tsx";
-//
-// export const indexRoute = createRoute({
-//     getParentRoute: () => rootRoute,
-//     path: '/',
-//     component: Index,
-// })
-//
-// function Index() {
-//     return (
-//         <div className="p-2">
-//             <h3>Welcome Home!</h3>
-//         </div>
-//     )
-// }
